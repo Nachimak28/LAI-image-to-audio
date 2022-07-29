0b4a6589cd00a8de9dd997e6188f8ddaac6a8dae
Nachiket <nachimak28@gmail.com>
2022-07-29 17:51:43 +0530

    np bug fix

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ class LitApp(L.LightningFlow):
     def run(self):
         demo_image_url = 'https://cdn.pixabay.com/photo/2022/07/25/10/19/mountain-7343375_960_720.jpg'
         image = Image.open(requests.get(demo_image_url, stream=True).raw)
-        self.LAI_image_to_audio.run(np.asarray(image))
+        self.LAI_image_to_audio.run(np.array(image))
         print(self.LAI_image_to_audio.output_audio_file_path)
+        print(self.LAI_image_to_audio.error_message)
 
 
 app = L.LightningApp(LitApp())

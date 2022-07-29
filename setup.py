b414081829b22b325c3dcde8fb74036c613d0105
Nachiket <nachimak28@gmail.com>
2022-07-29 16:48:34 +0530

    url change in setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="LAI_image_to_audio",
-    version="0.0.0",
+    version="0.0.1",
     description="⚡ Lightning component ⚡ generated with command: lightning init component",
     author="",
     author_email="",
     # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    url="https://github.com/Lightning-AI/lightning-component-template",
+    url="https://github.com/Nachimak28/LAI-image-to-audio",
     install_requires=[],
     packages=find_packages(),
 )

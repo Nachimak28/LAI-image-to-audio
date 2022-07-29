6b5d06ae596cba6ab16ce26ea003a8c6f43ab1df
Nachiket <nachimak28@gmail.com>
2022-07-29 17:47:54 +0530

    refactoring

--- LAI_image_to_audio/component.py
+++ LAI_image_to_audio/component.py
@@ -1,10 +1,7 @@
-import os
-import subprocess
-import numpy as np
 import lightning as L
-from PIL import Image, ImageOps
-from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTokenizer
-from utils import img_to_audio_obj
+from .utils import ImageToAudio
+
+image_to_audio_obj = ImageToAudio()
 
 
 class LitImgToAudio(L.LightningWork):
@@ -12,10 +9,8 @@ class LitImgToAudio(L.LightningWork):
         super().__init__()
         self.output_audio_file_path = ''
         self.error_message = None
-        self.image_to_audio_obj = img_to_audio_obj
 
     def run(self, image_np_array):
-        self.image_to_audio_obj.predict(image_np_array)
-        self.output_audio_file_path = self.image_to_audio_obj.output_audio_file_path
-        self.error_message = self.image_to_audio_obj.error_message
-
+        image_to_audio_obj.predict(image_np_array)
+        self.output_audio_file_path = image_to_audio_obj.output_audio_file_path
+        self.error_message = image_to_audio_obj.error_message

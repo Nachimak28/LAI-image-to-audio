e9b78684424ccd5791de77f379bb7f366b3eb823
Nachiket <nachimak28@gmail.com>
2022-07-29 16:29:42 +0530

    created a valid test

--- tests/test_LAI_image_to_audio_component.py
+++ tests/test_LAI_image_to_audio_component.py
@@ -5,9 +5,13 @@ To test a lightning component:
 2. call .run()
 """
 from LAI_image_to_audio.component import LitImgToAudio
-
+from PIL import Image
+import numpy as np
+import requests
 
 def test_placeholder_component():
-    messenger = LitImgToAudio()
-    messenger.run()
-    assert messenger.output_audio_file_path == './speech.wav'
+    lai_image_to_audio = LitImgToAudio()
+    demo_image_url = 'https://cdn.pixabay.com/photo/2022/07/25/10/19/mountain-7343375_960_720.jpg'
+    image = Image.open(requests.get(demo_image_url, stream=True).raw)
+    lai_image_to_audio.run(np.asarray(image))
+    assert lai_image_to_audio.output_audio_file_path == './speech.wav'
